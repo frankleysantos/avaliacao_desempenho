86bcd2ee57ee371accbfc4dcd0ed6404fdf34b25
frankleysantos <frankleysilvasantos@gmail.com>
2018-10-04 14:34:44 -0300

    avaliação de desempenho

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require "inc/cabecalho.php";
+require "inc/menu.php";
+?>
+1 - lista os funcionarios vinculados a cada gestor
+2 - Seleciona o funcionario que o gestor irá responder
+3 - Se o funcionario ja tiver sido avaliado, o botao de avaliar é desativado. (Ideia - listar os ativos e os não ativos separados)                                                  
+
+<?php
+require "inc/rodape.php";  
+?>
